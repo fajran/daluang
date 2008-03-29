dafcbf3c6904fc2be7ed6361499330d6bd6abfbb
Fajran Iman Rusadi <fajran@gmail.com>
2008-03-29 20:58:23 +0000

    Update setup.py
    git-svn-id: https://daluang.googlecode.com/svn/trunk@36 ad74ccad-ba46-0410-8d25-6dc0b50a7408

--- setup.py
+++ setup.py
@@ -3,17 +3,36 @@
 from distutils.core import setup
 
 setup(name='daluang',
-	version='0.1.1',
+	version='0.1.4',
 	description='Wikipedia Dump Reader',
 	author='Fajran Iman Rusadi',
 	author_email='fajran@gmail.com',
-	packages=['daluang', 'daluang.browser', 'daluang.server'],
+	packages=['daluang', 'daluang.browser', 'daluang.server', 'daluang.search'],
 	data_files=[
-		('bin', ['bin/daluang', 'bin/daluang-server', 'bin/daluang-browser']),
-		('share/daluang/example', ['example/daluang.conf']),
+		('bin', [
+			'bin/daluang', 
+			'bin/daluang-server', 
+			'bin/daluang-browser', 
+			'bin/daluang-browser-bin'
+		]),
+		('share/daluang/example', [
+			'example/daluang.conf'
+		]),
 		('share/daluang/data', []),
-		('share/daluang/browser/res', ['data/browser/res/browser.glade']),
-		('share/daluang/server/res', ['data/server/res/style.css']),
-		('share/daluang/server/tpl', ['data/server/tpl/index.html','data/server/tpl/article.html'])
+		('share/daluang/index', []),
+		('share/daluang/browser/res', [
+			'data/browser/res/browser.glade'
+		]),
+		('share/daluang/server/res', [
+			'data/server/res/style.css', 
+			'data/server/res/index.css',
+			'data/server/res/ext.png'
+		]),
+		('share/daluang/server/tpl', [
+			'data/server/tpl/index.html',
+			'data/server/tpl/article.html',
+			'data/server/tpl/search_result.html',
+			'data/server/tpl/not_found.html'
+		])
 	]
 )
